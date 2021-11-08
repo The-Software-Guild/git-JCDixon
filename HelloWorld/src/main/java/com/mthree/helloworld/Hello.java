33e6625671995d33457430f8f4ce54d784de8d31
John Dixon <sdxt5nis6@relay.firefox.com>
2021-11-08 15:02:27 -0500

    Initial HelloWorld

---
+++ HelloWorld/src/main/java/com/mthree/helloworld/Hello.java
@@ -0,0 +1,17 @@
+/**
+ *@author Your Name
+ *email: sdxt5nis6@relay.firefox.com
+ *date: 2021-11-08
+ *purpose: My first Hello World program in an IDE
+ */
+package com.mthree.helloworld;
+
+/**
+ *
+ * @author john
+ */
+public class Hello {
+    public static void main (String[] args){
+        System.out.println("Hello, World!");
+    }
+}
